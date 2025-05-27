f95fec1358575cb78e2e93fb3ec03698c8c4c44d
suporte <suporte@BG-LABX-PCX>
2025-05-27 19:44:41 -0300

    media

--- index.php
+++ index.php
@@ -21,3 +21,20 @@ dica: media aritimetica.
 
 tema de casa: calcular tambem as medias poderada e harmonica.
 */
+
+$mediaEscola = 7;
+
+$prova1 = 9;
+$prova2 = 7;
+$prova3 = 2;
+$prova4 = 2;
+
+$mediaAluno = ($prova1 + $prova2 + $prova3 + $prova4) / 4;
+
+if ($mediaAluno >= $mediaEscola) {
+    echo "O aluno foi aprovado com a media: $mediaAluno";
+} else {
+    echo "O aluno foi reprovado tendo a sua media: $mediaAluno menor que a media da escola: $mediaEscola";
+}
+
+// Criar uma calculadora
