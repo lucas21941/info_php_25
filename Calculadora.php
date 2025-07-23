896bc222960b12f6f99c86bc4fdb9c1b472aa007
suporte <suporte@BG-LABX-PCX.localdomain>
2025-07-22 21:53:33 -0300

    calculadora

---
+++ Calculadora.php
@@ -0,0 +1,28 @@
+<?php
+ 
+ class Calculadora {
+   public $resultado = 0;
+
+    public function somar($valor1, $valor2) {
+      $this->resultado = $valor1 + $valor2;
+    }   
+      public function diminuir($valor1, $valor2) {
+      $this->resultado = $valor1 - $valor2;
+    }
+     
+     public function multiplicar($valor1, $valor2) {
+      $this->resultado = $valor1 * $valor2;
+    }
+    
+     public function dividir($valor1, $valor2) {
+      $this->resultado = $valor1 / $valor2;
+    }
+
+    public function exibir() {
+      echo $this->resultado;
+    }
+ }
+
+ $objectCalculadora = new Calculadora();
+ $objectCalculadora->somar(10, 10);
+ $objectCalculadora->exibir();
